b4fa6c8a9e155c6012bd37a7c17635c12c24d78c
Sophia-sQ <s.quadros@aluno.ifsp.edu.br>
2026-06-24 08:52:16 -0300

    Update machine-learning.py

--- olimpiadas/OBI/CF-OBI2023/machine-learning.py
+++ olimpiadas/OBI/CF-OBI2023/machine-learning.py
@@ -40,34 +40,37 @@ print(x[maior])
 
 #SOLUÇÃO
 
-#N = int(input())
+#N = int(input()) #numero de topico/palavras
 #
-#dicionario = {}
-#cont = {}
+#dicionario = {} #Armazena palavra: tópico
+#cont = {} #Armazena o tópico como chave e o número de ocorrências no artigo como valor
 #
 #for _ in range(N):
-#    line = input().split()
-#    S = line[0]
-#    k = int(line[1])
+#    line = input().split() #linha tópico/palavras
+#    S = line[0] #tópico
+#    k = int(line[1]) #o numero de palavras relacionadas
 #    
-#    cont[S] = 0
-#    for i in range(2, len(line)):
-#        word = line[i]
-#        dicionario[word] = S
+#    cont[S] = 0 #chave é o topico cont[chave] é o número de ocorrências para esse tópico
+"""Faz a atribuição de cada palavra como chave e tópico como valor """
+#    for i in range(2, len(line)): #pula tópico e o inteiro e vai direto para as palavras atreladas
+#        word = line[i] #word=palavra
+#        dicionario[word] = S #dicionario-valor = tópico
 #
-#X = int(input())
+#X = int(input()) #número de palavras no artigo
 #maior_valor = 0
 #topico_do_artigo = ""
 #
 #artigo = input().split()
 #
 #for word in artigo:
-#
+#    #verifica se existe a chave word no dicionario
 #    if word not in dicionario:
 #        continue
-#
+#    #word é uma palavra no artigo
+"""a chaves de dicionario são as palavras relacionadas e o valor, os tópicos,
+quando retornar um valor com dicionario[word], essa é uma chave de cont""" 
 #    cont[dicionario[word]] += 1
-#
+#    #se é o maior numero e o topico do artigo e se topico artigo depois que chave (na ordem alfabética)
 #    if cont[dicionario[word]] > maior_valor or (cont[dicionario[word]] == maior_valor and topico_do_artigo > dicionario[word]):
 #        maior_valor = cont[dicionario[word]]
 #        topico_do_artigo = dicionario[word]
